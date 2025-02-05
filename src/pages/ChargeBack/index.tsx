052c4036b20a4d6d38963680abd4a23f052b1f67
aryan11814 <aryan181431@gmail.com>
2025-02-05 23:16:35 +0530

    Added modal chargeback and settlement

--- src/pages/ChargeBack/index.tsx
+++ src/pages/ChargeBack/index.tsx
@@ -18,7 +18,8 @@ function ChargeBack() {
   const [newChargeBackModal, setNewChargeBackModal] = useState(false);
   const sendButtonRef = useRef(null);
   const [selectedUser, setSelectedUser] = useState("1");
-
+  const [VerificationModal , setVerificationModal] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="grid grid-cols-12 gap-y-10 gap-x-6">
       <div className="col-span-12">
@@ -271,6 +272,61 @@ function ChargeBack() {
 
                       </Dialog.Panel>
                     </Dialog>
+                    <Dialog
+  open={VerificationModal}
+  onClose={() => setVerificationModal(false)}
+  initialFocus={sendButtonRef}
+>
+  <Dialog.Panel>
+    <Dialog.Title>
+      <h2 className="mr-auto text-base font-medium">Password Verification</h2>
+      <Lucide
+                                                        icon="X"
+                                                        className="w-5 h-5 ml-px stroke-[3]"
+                                                        onClick={() => setVerificationModal(false)}
+                                                    />
+                                                    <Menu className="sm:hidden">
+                                                        <Menu.Button
+                                                            as="a"
+                                                            className="block w-5 h-5"
+                                                            href="#"
+                                                        >
+                                                            <Lucide
+                                                                icon="MoreHorizontal"
+                                                                className="w-5 h-5 text-slate-500"
+                                                            />
+                                                        </Menu.Button>
+
+                                                    </Menu>
+    </Dialog.Title>
+    <fieldset className="col-span-12 sm:col-span-12 border-2 rounded-lg border-gray-200 mx-5 my-2">
+      <legend className="ml-4 pt-1 px-2">Password</legend>
+      <Dialog.Description>
+      <div className="relative col-span-12 sm:col-span-12">
+        <FormInput
+          id="modal-form-1"
+          placeholder="Type here..."
+          type={showPassword ? "text" : "password"} // Toggle password visibility
+          className="w-full pr-10" // Space for icon
+        />
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+        >
+          {showPassword ? <Lucide icon="EyeOff" /> : <Lucide icon="Eye" />}
+        </button>
+      </div>
+      </Dialog.Description>
+     
+    </fieldset>
+    <Dialog.Footer>
+      <Button variant="primary" type="button" className="w-20" ref={sendButtonRef}>
+        Verify
+      </Button>
+    </Dialog.Footer>
+  </Dialog.Panel>
+</Dialog>
                     {/* END: Modal Content */}
                   </Preview>
                 </>
@@ -506,21 +562,27 @@ function ChargeBack() {
                                 />
                               </Menu.Button>
                               <Menu.Items className="w-40">
-                                <Menu.Item>
-                                  <Lucide
-                                    icon="CheckSquare"
-                                    className="w-4 h-4 mr-2"
-                                  />{" "}
-                                  Edit
-                                </Menu.Item>
-                                <Menu.Item className="text-danger">
-                                  <Lucide
-                                    icon="Trash2"
-                                    className="w-4 h-4 mr-2"
-                                  />
-                                  Delete
-                                </Menu.Item>
-                              </Menu.Items>
+                              <Menu.Item  onClick={(event: React.MouseEvent) => {
+                                    event.preventDefault();
+                                    setVerificationModal(true);
+                                  }}>
+                                <Lucide
+                                  icon="CheckSquare"
+                                  className="w-4 h-4 mr-2"
+                                />{" "}
+                                Edit
+                              </Menu.Item>
+                              <Menu.Item className="text-danger"  onClick={(event: React.MouseEvent) => {
+                                    event.preventDefault();
+                                    setVerificationModal(true);
+                                  }}>
+                                <Lucide
+                                  icon="Trash2"
+                                  className="w-4 h-4 mr-2"
+                                />
+                                Delete
+                              </Menu.Item>
+                            </Menu.Items>
                             </Menu>
                           </div>
                         </Table.Td>
