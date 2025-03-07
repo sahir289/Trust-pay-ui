73d91770727cc25e3ab0cb1fa9eac6517118bbb2
Shadow <shadow77278837@gmail.com>
2025-03-07 17:46:25 +0400

    Handle Tabs VIA Redux

--- src/pages/TransactionList/Payout/payout.tsx
+++ src/pages/TransactionList/Payout/payout.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Tab } from '@/components/Base/Headless';
 import AllPayOut from './allPayout';
 import CompletedPayOut from './completedPayout';
@@ -8,12 +8,9 @@ import RejectedPayOut from './rejectedPayout';
 import Lucide from '@/components/Base/Lucide';
 import LoadingIcon from '@/components/Base/LoadingIcon';
 import { useAppDispatch } from '@/redux-toolkit/hooks/useAppDispatch';
-import { Status } from '@/constants';
-import Notification, { NotificationElement } from '@/components/Base/Notification';
-import { getAllPayOuts } from '@/redux-toolkit/slices/payout/payoutAPI';
-import { getPayOuts } from '@/redux-toolkit/slices/payout/payoutSlice';
 import { useAppSelector } from '@/redux-toolkit/hooks/useAppSelector';
-import { getPaginationData } from '@/redux-toolkit/slices/common/params/paramsSelector';
+import { setActiveTab } from '@/redux-toolkit/slices/common/tabs/tabSlice';
+import { getTabs } from '@/redux-toolkit/slices/common/tabs/tabSelectors';
 
 interface PayOutProps {
   // setModalData: React.Dispatch<
@@ -22,121 +19,73 @@ interface PayOutProps {
 }
 
 const PayOut: React.FC<PayOutProps> = () => {
-  const params = useAppSelector(getPaginationData);
-  const [notificationMessage, setNotificationMessage] = useState('');
-  const [notificationStatus, setNotificationStatus] = useState('');
-  // Basic non sticky notification
-  const basicNonStickyNotification = useRef<NotificationElement>();
-  const basicNonStickyNotificationToggle = () => {
-    // Show notification
-    basicNonStickyNotification.current?.showToast();
-  };
   const dispatch = useAppDispatch();
+  const activeTab = useAppSelector(getTabs); // Get active tab from state
 
-  useEffect(() => {
-    getPayOutData();
-  }, [JSON.stringify(params)]);
-
-  const getPayOutData = async () => {
-    const queryString = new URLSearchParams(params as Record<string, string>).toString();
-    const payOuts = await getAllPayOuts(queryString);
-    if (payOuts?.data) {
-      const payload = {
-        payout: payOuts.data.rows,
-        totalCount: payOuts.data.totalCount,
-      };
-      dispatch(getPayOuts(payload));
-    } else {
-      setNotificationStatus(Status.ERROR);
-      setNotificationMessage('No PayIns Found!');
-      basicNonStickyNotificationToggle();
-    }
+  const handleTabChange = (index: number) => {
+    dispatch(setActiveTab(index)); // Dispatch action to set the active tab
   };
 
   return (
-    <>
-      <div className="flex flex-col p-5">
-        <Tab.Group>
-          <Tab.List variant="tabs">
-            <Tab>
-              <Tab.Button
-                className="w-full py-2 flex items-center justify-center"
-                as="button"
-              >
-                <Lucide icon="Globe" className="w-5 h-5 ml-px stroke-[2.5]" />
-                &nbsp; All
-              </Tab.Button>
-            </Tab>
-            <Tab>
-              <Tab.Button
-                className="w-full py-2 flex items-center justify-center"
-                as="button"
-              >
-                <Lucide
-                  icon="BadgeCheck"
-                  className="w-5 h-5 ml-px stroke-[2.5]"
-                />
-                &nbsp; Completed
-              </Tab.Button>
-            </Tab>
-            <Tab>
-              <Tab.Button
-                className="w-full py-2 flex items-center justify-center"
-                as="button"
-              >
-                <LoadingIcon icon="ball-triangle" className="w-4 h-4" />
-                &nbsp; In Progress
-              </Tab.Button>
-            </Tab>
-            <Tab>
-              <Tab.Button
-                className="w-full py-2 flex items-center justify-center"
-                as="button"
-              >
-                <Lucide icon="BadgeX" className="w-5 h-5 ml-px stroke-[2.5]" />
-                &nbsp; Rejected
-              </Tab.Button>
-            </Tab>
-          </Tab.List>
-          <Tab.Panels className="border-b border-l border-r">
-            <Tab.Panel className="py-5 leading-relaxed">
-              <AllPayOut />
-            </Tab.Panel>
-            <Tab.Panel className="py-5 leading-relaxed">
-              <CompletedPayOut />
-            </Tab.Panel>
-            <Tab.Panel className="py-5 leading-relaxed">
-              <InProgressPayOut />
-            </Tab.Panel>
-            <Tab.Panel className="py-5 leading-relaxed">
-              <RejectedPayOut />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
-      </div>
-      {notificationMessage && (
-        <div className="text-center">
-          <Notification
-            getRef={(el) => {
-              basicNonStickyNotification.current = el;
-            }}
-            options={{
-              duration: 3000,
-            }}
-            className="flex flex-col sm:flex-row"
-          >
-            {notificationStatus === Status.SUCCESS ? (
-              <Lucide icon="BadgeCheck" className="text-primary" />
-            ) : (
-              <Lucide icon="X" className="text-danger" />
-            )}
-            <div className="font-medium ml-4 mr-4">
-              <div className="font-medium">{notificationMessage}</div>
-            </div>
-          </Notification>
-        </div>
-      )}
-    </>
+    <div className="flex flex-col p-5">
+      <Tab.Group selectedIndex={activeTab} onChange={handleTabChange}>
+        <Tab.List variant="tabs">
+          <Tab>
+            <Tab.Button
+              className="w-full py-2 flex items-center justify-center"
+              as="button"
+            >
+              <Lucide icon="Globe" className="w-5 h-5 ml-px stroke-[2.5]" />
+              &nbsp; All
+            </Tab.Button>
+          </Tab>
+          <Tab>
+            <Tab.Button
+              className="w-full py-2 flex items-center justify-center"
+              as="button"
+            >
+              <Lucide
+                icon="BadgeCheck"
+                className="w-5 h-5 ml-px stroke-[2.5]"
+              />
+              &nbsp; Completed
+            </Tab.Button>
+          </Tab>
+          <Tab>
+            <Tab.Button
+              className="w-full py-2 flex items-center justify-center"
+              as="button"
+            >
+              <LoadingIcon icon="ball-triangle" className="w-4 h-4" />
+              &nbsp; In Progress
+            </Tab.Button>
+          </Tab>
+          <Tab>
+            <Tab.Button
+              className="w-full py-2 flex items-center justify-center"
+              as="button"
+            >
+              <Lucide icon="BadgeX" className="w-5 h-5 ml-px stroke-[2.5]" />
+              &nbsp; Rejected
+            </Tab.Button>
+          </Tab>
+        </Tab.List>
+        <Tab.Panels className="border-b border-l border-r">
+          <Tab.Panel className="py-5 leading-relaxed">
+            <AllPayOut />
+          </Tab.Panel>
+          <Tab.Panel className="py-5 leading-relaxed">
+            <CompletedPayOut />
+          </Tab.Panel>
+          <Tab.Panel className="py-5 leading-relaxed">
+            <InProgressPayOut />
+          </Tab.Panel>
+          <Tab.Panel className="py-5 leading-relaxed">
+            <RejectedPayOut />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
   );
 };
 
--- src/pages/TransactionList/index.tsx
+++ src/pages/TransactionList/index.tsx
@@ -6,43 +6,37 @@ import Modal from '@/pages/Modal/modal';
 import Lucide from '@/components/Base/Lucide';
 import { useState, useRef } from 'react';
 import ModalPopUp from '../ModalPopUp';
-import Notification, {
-  NotificationElement,
-} from '@/components/Base/Notification';
+import Notification, { NotificationElement } from '@/components/Base/Notification';
 import { updatePayIns } from '@/redux-toolkit/slices/payin/payinAPI';
 import { Status } from '@/constants';
+import { useAppDispatch } from '@/redux-toolkit/hooks/useAppDispatch';
+import { useAppSelector } from '@/redux-toolkit/hooks/useAppSelector';
+import { getParentTabs } from '@/redux-toolkit/slices/common/tabs/tabSelectors';
+import { setParentTab } from '@/redux-toolkit/slices/common/tabs/tabSlice';
 
 function Main() {
+  const dispatch = useAppDispatch();
+  const parentTab = useAppSelector(getParentTabs); // Get parent tab from Redux
   const [newTransactionModal, setNewTransactionModal] = useState(false);
-  const [title, setTitle] = useState('PayIns');
+  const [title, setTitle] = useState(parentTab === 0 ? 'PayIns' : 'PayOuts');
   const [status, setStatus] = useState<string>('');
   const [id, setId] = useState<string>('');
   const [notificationMessage, setNotificationMessage] = useState('');
   const [notificationStatus, setNotificationStatus] = useState('');
-  const [modalData, setModalData] = useState<{ open: boolean; type: string }>({
-    open: false,
-    type: '',
-  });
-  const transactionRef = useRef(null);
+  const [modalData, setModalData] = useState<{ open: boolean; type: string }>({ open: false, type: '' });
 
   const notificationRef = useRef<NotificationElement>();
   const resetRef = useRef<null>(null);
+  const transactionRef = useRef(null);
 
   const toggleModal = () => setNewTransactionModal((prev) => !prev);
   const closeModal = () => setModalData({ open: false, type: '' });
 
   const handleSubmit = async (data: Record<string, string>) => {
-    const apiData =
-      status === Status.BANK_MISMATCH
-        ? { type: 'PAYIN', ...data }
-        : { ...data };
-
-    const url =
-      status === Status.BANK_MISMATCH
-        ? `/update-deposit-status/${id}`
-        : `/dispute-duplicate/${id}`;
-
+    const apiData = status === Status.BANK_MISMATCH ? { type: 'PAYIN', ...data } : { ...data };
+    const url = status === Status.BANK_MISMATCH ? `/update-deposit-status/${id}` : `/dispute-duplicate/${id}`;
     const res = await updatePayIns(url, apiData);
+
     if (res?.data?.data?.message) {
       setNotificationMessage(res.data.data.message);
       setNotificationStatus(Status.SUCCESS);
@@ -53,42 +47,38 @@ function Main() {
     notificationRef.current?.showToast();
   };
 
+  const handleParentTabChange = (index: number) => {
+    dispatch(setParentTab(index)); // Update Redux state
+    setTitle(index === 0 ? 'PayIns' : 'PayOuts');
+  };
+
   return (
     <>
       <div className="flex flex-col h-10 w-full px-2">
         <div className="flex justify-between items-center">
           <div className="text-xl font-medium">Transactions</div>
-          <Modal
-            handleModal={toggleModal}
-            sendButtonRef={transactionRef}
-            forOpen={newTransactionModal}
-            title={title}
-          />
+          <Modal handleModal={toggleModal} sendButtonRef={transactionRef} forOpen={newTransactionModal} title={title} />
         </div>
       </div>
       <div className="grid grid-cols-12 gap-6 mt-2">
         <div className="col-span-12">
           <div className="p-5 box box--stacked">
-            <Tab.Group>
+            <Tab.Group selectedIndex={parentTab} onChange={handleParentTabChange}>
               <Tab.List variant="tabs">
                 <Tab>
                   <Tab.Button
                     className="w-full py-2 flex items-center justify-center"
                     as="button"
-                    onClick={() => setTitle('PayIns')}
                   >
-                    <Lucide icon="BadgeIndianRupee" className="w-5 h-5" />{' '}
-                    &nbsp; PayIns
+                    <Lucide icon="BadgeIndianRupee" className="w-5 h-5" /> &nbsp; PayIns
                   </Tab.Button>
                 </Tab>
                 <Tab>
                   <Tab.Button
                     className="w-full py-2 flex items-center justify-center"
                     as="button"
-                    onClick={() => setTitle('PayOuts')}
                   >
-                    <Lucide icon="ArrowRightCircle" className="w-5 h-5" />{' '}
-                    &nbsp; PayOuts
+                    <Lucide icon="ArrowRightCircle" className="w-5 h-5" /> &nbsp; PayOuts
                   </Tab.Button>
                 </Tab>
               </Tab.List>
@@ -112,72 +102,19 @@ function Main() {
           title="Update Transaction"
           fields={
             modalData.type === 'approve'
-              ? [
-                  {
-                    id: 'method',
-                    label: 'Method',
-                    type: 'text',
-                    placeholder: 'Method',
-                  },
-                  {
-                    id: 'selectBank',
-                    label: 'Select Bank',
-                    type: 'text',
-                    placeholder: 'Select Bank',
-                  },
-                ]
+              ? [{ id: 'method', label: 'Method', type: 'text', placeholder: 'Method' }]
               : modalData.type === 'reject'
               ? []
-              : [
-                  {
-                    id: 'amount',
-                    label: 'Amount',
-                    type: 'text',
-                    placeholder: 'Amount',
-                  },
-                  {
-                    id: 'confirmed',
-                    label: 'Confirm Amount',
-                    type: 'text',
-                    placeholder: 'Confirm Amount',
-                  },
-                ]
+              : [{ id: 'amount', label: 'Amount', type: 'text', placeholder: 'Amount' }]
           }
           singleField={
             modalData.type === 'approve'
-              ? [
-                  {
-                    id: 'utrNumber',
-                    label: 'UTR Number',
-                    type: 'text',
-                    placeholder: 'UTR Number',
-                  },
-                ]
+              ? [{ id: 'utrNumber', label: 'UTR Number', type: 'text', placeholder: 'UTR Number' }]
               : modalData.type === 'reject'
-              ? [
-                  {
-                    id: 'rejectReason',
-                    label: 'Reject Reason',
-                    type: 'text',
-                    placeholder: 'Reject Reason',
-                  },
-                ]
-              : [
-                  {
-                    id: 'merchantOrderId',
-                    label: 'Merchant Order ID',
-                    type: 'text',
-                    placeholder: 'Merchant Order ID',
-                  },
-                ]
+              ? [{ id: 'rejectReason', label: 'Reject Reason', type: 'text', placeholder: 'Reject Reason' }]
+              : [{ id: 'merchantOrderId', label: 'Merchant Order ID', type: 'text', placeholder: 'Merchant Order ID' }]
           }
-          buttonText={
-            modalData.type === 'approve'
-              ? 'Approve'
-              : modalData.type === 'reject'
-              ? 'Reject'
-              : 'Success'
-          }
+          buttonText={modalData.type === 'approve' ? 'Approve' : modalData.type === 'reject' ? 'Reject' : 'Success'}
           onSubmit={handleSubmit}
           onReset={closeModal}
           resetRef={resetRef}
@@ -186,18 +123,8 @@ function Main() {
 
       {notificationMessage && (
         <div className="text-center">
-          <Notification
-            getRef={(el) => (notificationRef.current = el)}
-            options={{ duration: 3000 }}
-          >
-            <Lucide
-              icon={notificationStatus === Status.SUCCESS ? 'BadgeCheck' : 'X'}
-              className={
-                notificationStatus === Status.SUCCESS
-                  ? 'text-primary'
-                  : 'text-danger'
-              }
-            />
+          <Notification getRef={(el) => (notificationRef.current = el)} options={{ duration: 3000 }}>
+            <Lucide icon={notificationStatus === Status.SUCCESS ? 'BadgeCheck' : 'X'} className={notificationStatus === Status.SUCCESS ? 'text-primary' : 'text-danger'} />
             <div className="ml-4 font-medium">{notificationMessage}</div>
           </Notification>
         </div>
